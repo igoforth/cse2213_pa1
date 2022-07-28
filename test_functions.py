b5597e05551d01c02a2283dfcc76e20b5e2c7859
Levi Gist <71097489+lgist2@users.noreply.github.com>
2022-07-28 07:55:35 -0500

    added tests
    added tests to displayItem and openFile 
    also added comments

--- test_functions.py
+++ test_functions.py
@@ -15,6 +15,12 @@ def test_openFile(capsys):
     openFile("share.txt")
     captured_stdout, captured_stderr = capsys.readouterr()
     assert captured_stdout.strip() == "File not found"
+
+#FNF error
+def test_openFile(capsys):
+    openFile("")
+    captured_stdout, captured_stderr = capsys.readouterr()
+    assert captured_stdout.strip() == "File not found"
     
 
 
@@ -22,6 +28,7 @@ def test_openFile(capsys):
 #testing division with int and floats
 #passes first 2, fails 3rd bc it isn't equal -> create if doesn't equal
 #4th fails due to decimal rounding
+#5th fails due to TypeError -> cannot be a string
 @pytest.mark.parametrize("num1,num2,div",[(4,2,2),(8.6,4.3,2),(42,7,7),(45,23,1.9),('6','3',2)])
 def test_numbers(num1, num2,div):
     assert numbers(num1,num2) == div
@@ -76,7 +83,7 @@ def test_sq(num,out):
 
 
 #greetUser 
-#all pass, 
+#all pass because all can identify as strings
 #testing input and output
 @pytest.mark.parametrize("first,middle,last,expected",[("Peter", "Benjamin", "Parker","Hello!\nWelcome to the program Peter Benjamin Parker\nGlad to have you!"),(1,2,3,"Hello!\nWelcome to the program 1 2 3\nGlad to have you!"), ("Walter", "Pensky", "Jackson IV","Hello!\nWelcome to the program Walter Pensky Jackson IV\nGlad to have you!")])
 def test_greetUser(capsys,first,middle,last,expected):
@@ -97,11 +104,14 @@ def test_greetUser2(capsys,first,middle,last,expected):
 #displayItem
 #first passes
 #second fails -> IndexError -> out of range
-#third fails -> IndesError -> should not have a negative index
-@pytest.mark.parametrize("numbers, index,expected",[([1,5,9,8],2,"Your item at 2 index is 9"),([5,7,6,3,1],5,"Your item at 5 index is 0"),([9,6,2,1,99],-9,"Your item at -4 index is 0")])
+#third fails -> IndexError -> should not have a negative index
+#fourth fails -> TypeError -> cannot be a float
+#fifth fails -> TypeError -> cannot be a string
+@pytest.mark.parametrize("numbers, index,expected",[([1,5,9,8],2,"Your item at 2 index is 9"),([5,7,6,3,1],5,"Your item at 5 index is 0"),([9,6,2,1,99],-9,"Your item at -4 index is 0"),([8,7,2,8],2.5,"Your item at 2.5 index is 0"),([7,4,0,1],'1',"Your item at 1 index is 4")])
 def test_displayItem(capsys,numbers,index,expected):
     displayItem(numbers,index)
 
     captured_stdout, captured_stderr = capsys.readouterr()
     assert captured_stdout.strip() == expected
+
 

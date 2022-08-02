da1514f1262adddae51fe905d4ee9113586bf94c
Ian Goforth <ian.goforth@gmail.com>
2022-08-02 17:10:23 -0500

    Database is functional. See demo in main.py

--- database.py
+++ database.py
@@ -1,27 +1,104 @@
+import os
+import io
 import json
+import jsonpickle
 
-def initialize():
-    return
+class Database:
+    def __init__(self):
+        self.file = open("database/database.json", "w")
+        self.users = {}
+        self.catalog = Catalog()
+    
+    def __enter__(self):
+        if os.stat("database/database.json").st_size != 0:
+                self = jsonpickle.decode(self.file.read())
+        return self
 
-def database(mode, data):
-    modes = ["user", "pay", "cart", "catalog", "history", "shipping"]
-    return
+    def __exit__(self, ctx_type, ctx_value, ctx_traceback):
+        try:
+            self.file.write(jsonpickle.encode(self))
+        except Exception as e:
+            print("Error writing to database: {}".format(e))
+            return False
+        self.file.close()
 
-def catalogGet():
-    catalog = open("database/catalog.txt", "r")
-    for line in catalog.readline():
-        
-        return
-    catalog.close()
+    def userGet(self, username="", password=""):
+        result = {}
+        for key, value in self.users.items():
+            if key == username or value.password == password:
+                result[key] = value
+        return result
 
-def catalogSet(id, name, price, category):
-    catalog = open("database.json", "w")
-    catalog.write(id + " " + name + " " + price + " " + category)
-    catalog.close()
+    def userSet(self, username, password):
+        self.users[username] = User(password)
 
-class Item:
-    def __init__(self, id, name, price, category):
-        self.id = id
+class Catalog():
+    def __init__(self):
+        self.items = {}
+
+    def itemGet(self, id=None, name="", price="", category=""):
+        result = {}
+        for key, value in self.items.items():
+            if key == id or value.name == name or value.price == price or value.category == category:
+                result[key] = value
+        return result
+
+    def itemSet(self, id, name, price, category):
+        self.items[id] = Item(name, price, category)
+
+class User():
+    def __init__(self, password):
+        self.password = password
+        self.cart = Cart()
+        self.history = History()
+
+    def shippingGet(self):
+        return self.shipping
+
+    def shippingSet(self, street, state, city, zip):
+        self.shipping = Ship(street, state, city, zip)
+
+    def paymentGet(self):
+        return self.payment
+
+    def paymentSet(self, card_num, exp_date, csv):
+        self.payment = Pay(card_num, exp_date, csv)
+
+class Ship():
+    def __init__(self, street, state, city, zip):
+        self.street = street
+        self.state = state
+        self.city = city
+        self.zip = zip
+
+class Pay():
+    def __init__(self, card_num, exp_date, csv):
+        self.card_num = card_num
+        self.exp_date = exp_date
+        self.csv = csv
+
+class Cart():
+    def __init__(self):
+        self.items = []
+    
+    def itemsGet(self):
+        return self.items
+
+    def itemSet(self, name, price, category):
+        self.items.append(Item(name, price, category))
+
+class History():
+    def __init__(self):
+        self.items = []
+    
+    def itemsGet(self):
+        return self.items
+
+    def itemSet(self, id, name, price, category):
+        self.items.append(Item(id, name, price, category))
+
+class Item():
+    def __init__(self, name, price, category):
         self.name = name
         self.price = price
         self.category = category
--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-def main():
-    login =1
-    while login!= 0:
-        print("\n1.Sign in"
-        "\n2.Sign up"
-        "\n3.Exit")
-        answer = input("~~~:")
+from database import Database
 
-        if answer == "1":
-            username = input("Enter in a username:~~~~")
-            password = input("enter in a password:~~~~")
-            #if username = a username and if password = a password
-                #login = 0
-                #app.py
-        elif answer =="2":
+
+def main():
+    with Database() as d:
+        login = 1
+        while login!= 0:
+            print("\n1.Sign in"
+            "\n2.Sign up"
+            "\n3.Exit")
+            answer = input("~~~:")
+
+            if answer == "1":
+                username = input("Enter in a username:~~~~")
+                password = input("Enter in a password:~~~~")
+                if len(d.userGet(username=username, password=password)) > 0:
+                    print("Welcome back, {}!".format(username))
+                    login = 0
+                else:
+                    print("Username or password is incorrect")
+            elif answer =="2":
                 username = input("Enter in a desired username:~~~~~")
                 password = input("Enter in a desired password:~~~~~")
-        else:
-            exit()
+                d.userSet(username, password)
+                print("Result is:", d.userGet(username=username, password=password))
+            else:
+                d.close()
+                exit()
 
 main()
